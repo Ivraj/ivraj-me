4f9592c9288aa8640136003ad4912295059d9703
Ivraj <iseerha@gmail.com>
2020-07-25 02:10:21 -0500

    Switches 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,16 @@
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+import { Link } from "gatsby"
+
+const errorPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>NOT FOUND</h1>
+    <p>
+      You just hit a route that doesn&#39;t exist. Darn.{" "}
+      <Link to="/">Let's head home.</Link>
+    </p>
+  </Layout>
+)
+
+export default errorPage
