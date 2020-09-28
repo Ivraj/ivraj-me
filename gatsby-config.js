63eb56d3854dd33514941606f71fcbd8d5507bf1
ivraj <iseerha@gmail.com>
2020-09-28 11:27:52 -0500

    Adds GA tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,12 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-146418426-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
